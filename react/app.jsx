14df6ceeca2509de01d7b0da079396aaba2df4d8
tuyen nguyen <killerdaumat@gmail.com>
2022-11-02 05:46:19 +0700

    .

--- react/app.jsx
+++ react/app.jsx
@@ -1,47 +1,46 @@
-
-// let myName="Tuyen"
-// let isdeptrai=true
-
-// function createApp(){
-//   const now=new Date();
-//   return (
-//     <div className="tuyen">
-//         <h1 className="heading">{myName} ok</h1>
-//         <p>bay gio la {now.toLocaleTimeString()}</p>
-//         {isdeptrai && <div>
-//                  tuyen dep trai
-//           </div>}
-//         <img src="https://image-us.24h.com.vn/upload/2-2021/images/2021-05-22/anh-8-1621645023-458-width650height813.jpg" alt="" />
-//         <p>{myName} dep trai vl</p>
-//       </div>
-    
-//   )
-// }
-// const root = ReactDOM.createRoot(document.querySelector("#app"));
-// setInterval(()=>{
-//   root.render(createApp());
-// },1000)
-function Time(){
-const now=new Date();
-return <p> it {now.toLocaleDateString()}</p>
+function Header(){
+    return(
+        <div className="heading"> 
+          <h1>Our Reviews</h1>
+          <div className="line">
+          </div>
+        </div>
+    )
 }
-function Hello({user}){
-  return <h1>{user.name},{user.age} dep trai</h1>
+function ReviewsCard(){
+    return (
+        <div className="reviewscard">
+            <div className="author">
+                <div className="avatar">
+                  <img src="https://fauto.vn/wp-content/uploads/2022/08/anh-sexy-8.jpg" alt="" />
+                  <div className="phay">
+                  <i class="bi bi-info-circle"></i>
+                  </div>
+                </div>
+                <div className="info">
+                 <h2>Mr Tuyen</h2>
+                 <h2>Web Development</h2>
+                </div>
+            </div>
+            <div className="content">
+             <p>React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.</p>
+
+            </div>
+            <div className="control">
+            <i class="bi bi-arrow-left-circle-fill"></i>
+            <i class="bi bi-arrow-right-circle-fill"></i>
+            </div>
+        </div>
+    )
 }
+
 function App(){
-  let user1={
-    name:"tuyenok",
-    age:22,
-  }
-  return <div>
-    <Hello user={user1}/>
-    <Time/>
-    <Time/>
-    <Time/>
-    <Time/>
-    <Time/>
-  </div>
+    return (
+        <div className="appcontent">
+            <Header/>
+            <ReviewsCard/>
+        </div>
+    )
 }
-
 const root=ReactDOM.createRoot(document.querySelector("#app"))
 root.render(App())
